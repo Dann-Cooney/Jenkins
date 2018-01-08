67737c793efbf6403248cea2473941705d04efce
Dann-Cooney <32134227+Dann-Cooney@users.noreply.github.com>
2018-01-08 16:07:50 +0000

    Update Student.java

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -4,14 +4,13 @@
  */
 public class Student {
 
-    public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
+    public enum AttendanceGrade {ABSENT, VERY_POOR,AVERAGE, GOOD, VERY_GOOD};
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
-        if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
-        if (attendance <= 90) return AttendanceGrade.GOOD;
+        if (attendance < 90) return AttendanceGrade.GOOD;
         return AttendanceGrade.VERY_GOOD;
     }
 }
